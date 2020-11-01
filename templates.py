3095040374396bb0681826f26eb7eb403cce0e9c
cmehta <chetanmehta0204@gmail.com>
2020-11-01 18:48:32 +0530

    metrics calculation and template update.

--- templates.py
+++ templates.py
@@ -21,11 +21,13 @@ def set_template(args):
         args.train_negative_sample_size = 0
         args.train_negative_sampling_seed = 0
         args.test_negative_sampler_code = 'random'
-        args.test_negative_sample_size = 100
+        # args.test_negative_sample_size = 100
+        args.test_negative_sample_size = 40
         args.test_negative_sampling_seed = 98765
 
         args.trainer_code = 'bert'
         args.device = 'cuda'
+        # args.device = 'cpu'
         args.num_gpu = 4
         args.device_idx = '0,1,2,3'
         args.optimizer = 'Adam'
@@ -34,20 +36,22 @@ def set_template(args):
         args.enable_lr_schedule = True
         args.decay_step = 25
         args.gamma = 1.0
-        # args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
-        args.num_epochs = 100
-        args.metric_ks = [1, 5, 10, 20, 50, 100]
+        args.num_epochs = 100 if args.dataset_code == 'ml-1m' else 200
+        # args.metric_ks = [1, 5, 10, 20, 50, 100]
+        args.metric_ks = [1, 5, 10, 20, 40]
         args.best_metric = 'NDCG@10'
 
         args.model_code = 'bert'
         args.model_init_seed = 0
 
         args.bert_dropout = 0.1
-        args.bert_hidden_units = 256
+        # args.bert_hidden_units = 256
+        args.bert_hidden_units = 128
         args.bert_mask_prob = 0.15
-        args.bert_max_len = 100
-        args.bert_num_blocks = 2
-        args.bert_num_heads = 4
+        # args.bert_max_len = 100
+        args.bert_max_len = 40
+        args.bert_num_blocks = 1
+        args.bert_num_heads = 2
     
     elif args.template.startswith('train_dae'):
         args.mode = 'train'
